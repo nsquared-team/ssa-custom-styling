d5428bb8b149f19d53237ed6015cad7406cb9395
Natalie Garza <nataliegarza011@gmail.com>
2024-06-17 08:24:29 -0500

    Create customize.php

---
+++ customize.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Plugin Name: SSA Customization - SCOR v6
+ * Plugin URI:  https://simplyscheduleappointments.com
+ * Description: Customizations to SSA for SCOR
+ * Version:     1.0.6
+ * Author:      Simply Schedule Appointments
+ * Author URI:  https://simplyscheduleappointments.com
+ * Donate link: https://simplyscheduleappointments.com
+ * License:     GPLv2
+ * Text Domain: simply-schedule-appointments
+ * Domain Path: /languages
+ *
+ * @link    https://simplyscheduleappointments.com
+ *
+ * @package Simply_Schedule_Appointments
+ * @version 1.0.6
+ *
+ */
+
+/**
+ * Copyright (c) 2019 Simply Schedule Appointments (email : support@simplyscheduleappointments.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2 or, at
+ * your discretion, any later version, as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1401  USA
+ */
